619fb6d1cf592eeb18f7ea269c4e07c5ae843711
xredon <xavier.redon.h@gmail.com>
2023-11-03 01:03:17 +0100

    Update script.js
    V2

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
-// Function to calculate the distance between two points using the Haversine formula
-function calculateDistance(lat1, lon1, lat2, lon2) {
-    const R = 6371; // Radius of the Earth in kilometers
-    const dLat = (lat2 - lat1) * (Math.PI / 180);
-    const dLon = (lon2 - lon1) * (Math.PI / 180);
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c * 1000;
-}
-
-// Function to update the distance on the webpage
+// Function to update the distance on the webpage and show the "Congratulations" message
 function updateDistance(position) {
     const currentLocation = position.coords;
-    const definedPoint = { lat: 41.478897, lon: 2.310161}; // Replace with your defined point's coordinates
+    const definedPoint = { lat: 41.3, lon: 2.1 }; // Replace with your defined point's coordinates
     const distance = calculateDistance(
         currentLocation.latitude,
         currentLocation.longitude,
         definedPoint.lat,
         definedPoint.lon
-    );
-    document.getElementById("currentLocation").textContent = `[${currentLocation.latitude}, ${currentLocation.longitude}]`;
-//    document.getElementById("definedPoint").textContent = `[${definedPoint.lat}, ${definedPoint.lon}]`;
-    document.getElementById("distance").textContent = distance.toFixed(0); // Display the distance with no decimal places
-}
+    ) * 1000; // Convert distance to meters
 
-// Function to handle geolocation errors
-function handleLocationError(error) {
-    console.error("Error getting location:", error.message);
-}
+    // Display the distance in meters at the center of the screen
+    const distanceElement = document.getElementById("distance");
+    distanceElement.textContent = distance.toFixed(2) + " meters";
 
-// Check if geolocation is available
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(updateDistance, handleLocationError);
-} else {
-    console.error("Geolocation is not available in your browser.");
+    // Show the "Congratulations" message if the distance is less than 3 meters
+    if (distance < 3) {
+        const congratsMessage = document.getElementById("congratsMessage");
+        congratsMessage.textContent = "Congratulations, you reached your destination!";
+    } else {
+        const congratsMessage = document.getElementById("congratsMessage");
+        congratsMessage.textContent = "";
+    }
 }
