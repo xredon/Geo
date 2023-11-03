abbfaede796a23f099b15e2b949d6a2664557cdd
xredon <xavier.redon.h@gmail.com>
2023-11-03 14:49:43 +0100

    Santornem-hi sense refresh

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+// Function to calculate the distance between two points using the Haversine formula
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Radius of the Earth in kilometers
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+}
+
 // Function to update the distance on the webpage and show the "Congratulations" message
 function updateDistance(position) {
     const currentLocation = position.coords;
-    const definedPoint = { lat: 41.478357, lon: 2.311172  }; // Replace with your defined point's coordinates
+    const definedPoint = { lat: yourDefinedLatitude, lon: yourDefinedLongitude }; // Replace with your defined point's coordinates
     const distance = calculateDistance(
         currentLocation.latitude,
         currentLocation.longitude,
@@ -23,22 +35,9 @@ function updateDistance(position) {
     }
 }
 
-// Function to refresh the location
-function refreshLocation() {
-    // Disable the refresh button to prevent multiple clicks
-    const refreshButton = document.getElementById("refreshButton");
-    refreshButton.disabled = true;
-
-    // Clear the existing distance and "Congratulations" message
-    document.getElementById("distance").textContent = "Calculating...";
-    document.getElementById("congratsMessage").textContent = "";
-
-    // Request the updated location
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(updateDistance, handleLocationError);
-    } else {
-        console.error("Geolocation is not available in your browser.");
-    }
+// Function to handle geolocation errors
+function handleLocationError(error) {
+    console.error("Error getting location:", error.message);
 }
 
 // Check if geolocation is available
@@ -47,32 +46,3 @@ if ("geolocation" in navigator) {
 } else {
     console.error("Geolocation is not available in your browser.");
 }
-
-// Add an event listener to the refresh button
-const refreshButton = document.getElementById("refreshButton");
-refreshButton.addEventListener("click", refreshLocation);
-
-
-
-// Function to calculate the distance between two points using the Haversine formula
-function calculateDistance(lat1, lon1, lat2, lon2) {
-    const R = 6371; // Radius of the Earth in kilometers
-    const dLat = (lat2 - lat1) * (Math.PI / 180);
-    const dLon = (lon2 - lon1) * (Math.PI / 180);
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c;
-}
-
-// Function to handle geolocation errors
-function handleLocationError(error) {
-    // Your existing code for handling geolocation errors
-     console.error("Error getting location:", error.message);
-    // Enable the refresh button
-    const refreshButton = document.getElementById("refreshButton");
-    refreshButton.disabled = false;
-}
-
-
