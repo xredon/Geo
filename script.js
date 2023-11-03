13c9fc2fdb9c7edc6cdad2989a0e867ec4e6fd00
xredon <xavier.redon.h@gmail.com>
2023-11-03 12:16:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Function to update the distance on the webpage and show the "Congratulations" message
 function updateDistance(position) {
     const currentLocation = position.coords;
-    const definedPoint = { lat: 41.4791443, lon: 2.3093515 }; // Replace with your defined point's coordinates
+    const definedPoint = { lat: 41.4791443, lon: 2.3093515  }; // Replace with your defined point's coordinates
     const distance = calculateDistance(
         currentLocation.latitude,
         currentLocation.longitude,
         definedPoint.lat,
         definedPoint.lon
-    ) * 1000; // Convert distance to meters "";
+    ) * 1000; // Convert distance to meters
 
-    document.getElementById("distance").textContent = distance.toFixed(2); // Display the distance with two decimal 
+    // Display the distance in meters at the center of the screen
+    const distanceElement = document.getElementById("distance");
+    distanceElement.textContent = distance.toFixed(2) + " meters";
 
     // Show the "Congratulations" message if the distance is less than 3 meters
     if (distance < 3) {
